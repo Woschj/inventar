498360148bffe2bdc98530074cd9d12770a55d1c
Woschj <81321922+Woschj@users.noreply.github.com>
2024-11-30 22:36:28 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,12 @@ pip install -r requirements.txt
 # Install gunicorn explicitly
 pip install gunicorn
 
-# Make sure gunicorn is in PATH
-export PATH="/opt/render/project/venv/bin:$PATH"
+# Print Python and pip locations
+which python
+which pip
+which gunicorn
+
+# Add to PATH
+export PATH="$PATH:/opt/render/project/.venv/bin"
 
 # Hier können weitere Build-Schritte hinzugefügt werden 
