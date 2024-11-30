9f87885fd32885df8d9431eb1f7f001e4beb4b2c
Woschj <81321922+Woschj@users.noreply.github.com>
2024-11-30 21:46:25 +0100

    new

--- build.sh
+++ build.sh
@@ -2,7 +2,16 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip
 python -m pip install --upgrade pip
+
+# Install requirements
 pip install -r requirements.txt
+
+# Install gunicorn explicitly
+pip install gunicorn
+
+# Make sure gunicorn is in PATH
+export PATH="/opt/render/project/venv/bin:$PATH"
 
 # Hier können weitere Build-Schritte hinzugefügt werden 
