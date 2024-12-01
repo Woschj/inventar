5e982af80e32d0c00655c2bc546ab6f66b96ec4d
Woschj <81321922+Woschj@users.noreply.github.com>
2024-12-01 08:34:35 +0100

    0.11

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,22 +1,21 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('my-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/static/css/styles.css',
-                '/static/js/main.js',
-                '/static/images/logo.png',
-                '/index.html',
-                '/manage_workers.html'
-            ]);
-        })
-    );
+const CACHE_NAME = 'inventar-app-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/style.css',
+  '/static/js/main.js',
+  // Füge hier weitere wichtige URLs hinzu
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
